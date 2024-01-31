48f60c41a747e5ac5bfd21039168eebbcd5a2369
JackSheepe <teterkinvklol@mail.ru>
2024-01-31 19:52:18 +0700

    small style changes

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -26,7 +26,7 @@ function RepoCard({ repo }: { repo: InfRepo }) {
   return (
     <div className="border py-2 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
       <a href={repo.html_url} target="_blank">
-        <h2 className="text-lg font-bold">{repo.full_name}</h2>
+        <h2 className="text-lg font-bold truncate">{repo.full_name}</h2>
         <p className="text-sm">
           Forks: <span className="font-bold mr-2">{repo.forks}</span>
           Watchers: <span className="font-bold">{repo.watchers}</span>
